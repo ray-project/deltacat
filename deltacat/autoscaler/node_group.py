6b0f3df10c7f72ab15ce9cc240a87a0bf321dc24
Jialin Liu <rootliu@amazon.com>
2022-11-08 15:53:48 -0800

    update placement group

--- deltacat/autoscaler/node_group.py
+++ deltacat/autoscaler/node_group.py
@@ -2,7 +2,21 @@ import ray
 import re
 import time
 import yaml
+import logging
 from typing import Optional, Union, List, Dict, Any, Callable, Tuple
+from ray.util.placement_group import (
+	placement_group,
+	placement_group_table,
+	remove_placement_group,
+	get_current_placement_group
+)
+
+from ray.util.scheduling_strategies import PlacementGroupSchedulingStrategy
+from ray.experimental.state.api import list_placement_groups, get_node, get_placement_group
+
+
+from deltacat import logs
+logger = logs.configure_deltacat_logger(logging.getLogger(__name__))
 
 class NodeGroupManager():
 
@@ -141,3 +155,76 @@ class NodeGroupManager():
 			print("There is no available resources for %s"%gname)
 			return None
 		return group_res
+
+
+class PlacementGroupManager():
+	"""Placement Group Manager
+	Create a list of placement group with the desired number of cpus
+	e.g., create a pg with 32 cpus, then this class will look for a node that has 32 cpus, and collect all 
+	resources, including cpu, memory, and object store;
+	How to use:
+		```
+			from deltacat.autoscaler.node_group import PlacementGroupManager as pgm
+			pgm = pgm(10, 32)
+			pg_configs = pgm.pgs
+			opts = pg_configs[0][0]
+			fun.options(**opts).remote()
+		```
+	Args:
+		num_pgs: number of placement groups to be created
+		instance_cpus: number of cpus per instance
+	"""
+	def __init__(self, num_pgs: int, instance_cpus: int, time_out: Optional[float] = None):
+		self._pg_configs =self._config(num_pgs, instance_cpus, time_out)
+	@property
+	def pgs(self):
+		return self._pg_configs
+
+	def _config(self,num_pgs: int, instance_cpus: int, time_out: Optional[float] = None) -> List[Tuple[Dict[str,Any], Dict[str,Any]]]:
+		for i in range(num_pgs):
+			pg_configs = []
+			try:
+				opts ={}
+				cluster_resources={}
+				bundle1={'CPU': instance_cpus} # hardcode for now
+				pg = placement_group([bundle1], strategy="PACK")
+				ray.get(pg.ready(), timeout=time_out)
+				if not pg:
+					continue
+				opts = {"scheduling_strategy":PlacementGroupSchedulingStrategy(
+					placement_group=pg, placement_group_capture_child_tasks=True)
+				}
+				pg_id = placement_group_table(pg)['placement_group_id']
+				#print("pg id{}".format(pg_id))
+				pg_details = get_placement_group(pg_id)
+				#print("pgs-{}".format(pgs))
+				bundles = pg_details['bundles']
+				node_ids =[]
+				for bd in bundles:
+					node_ids.append(bd['node_id'])
+				#query available resources given list of node id
+				all_nodes_available_res = ray._private.state.state._available_resources_per_node()
+				pg_res = {'CPU':0,'memory':0,'object_store_memory':0,'node_id':[]}
+				for node_id in node_ids:
+					if node_id in all_nodes_available_res:
+						v = all_nodes_available_res[node_id]
+						node_detail = get_node(node_id)
+						pg_res['CPU']+=node_detail['resources_total']['CPU']
+						pg_res['memory']+=v['memory']
+						pg_res['object_store_memory']+=v['object_store_memory']
+							#pg_res['node_id'].append(node_id)
+				cluster_resources['CPU'] = int(pg_res['CPU'])
+				cluster_resources['memory'] = float(pg_res['memory'])
+				cluster_resources['object_store_memory'] = float(pg_res['object_store_memory'])
+				#cluster_resources['node_id'] = pg_res['node_id']
+				#cluster_cpus = cluster_resources['CPU']
+				pg_configs.append([opts,cluster_resources])
+				print("{}-th pg has resources:{}".format(i,cluster_resources))
+
+			except Exception as e:
+				print("Getting error in creating {}-th pg:{}".format(i,e))
+				logger.error(f"placement group error{e}")
+				pass
+		return pg_configs	
+
+
