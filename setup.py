4ba13fc591bb02403d1563b3e3dce5e3ac4590b8
Pierce Faraone <faraonep@amazon.com>
2022-12-21 15:39:58 +0800

    Revert "added hello world"
    This reverts commit 1ed905f3dbcc9e1c202fca399864048c97dfd868.

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ logger = logging.getLogger(__name__)
 
 ROOT_DIR = os.path.dirname(__file__)
 
-print("hello pierce faraone faraone")
 
 def find_version(*paths):
     version_file_path = os.path.join(ROOT_DIR, *paths)
