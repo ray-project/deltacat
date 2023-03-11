5073ef0278f323788829e3df77adb96c5935c82a
jialin <valiantljk@gmail.com>
2023-03-10 23:04:25 -0800

    Compaction with no primary key index

--- deltacat/compute/compactor/model/round_completion_info.py
+++ deltacat/compute/compactor/model/round_completion_info.py
@@ -1,24 +1,24 @@
 # Allow classes to use self-referencing Type hints in Python 3.7.
 from __future__ import annotations
 
-from deltacat.storage import DeltaLocator, PartitionLocator
+from typing import Any, Dict, Optional
+
+from deltacat.compute.compactor.model.primary_key_index import (
+    PrimaryKeyIndexVersionLocator,
+)
 from deltacat.compute.compactor.model.pyarrow_write_result import PyArrowWriteResult
-from typing import Any, Dict, Optional, Union
+from deltacat.storage import DeltaLocator, PartitionLocator
 
 
 class RoundCompletionInfo(dict):
-    """
-    In case of compacting deltas from multi-sources, the high_watermark dict records the high watermark for each source
-    e.g., high_watermark = {"src1":100,"src2":200}
-    src1 or src2 is the canonical_string of partition_locator
-    """
-
     @staticmethod
     def of(
-        high_watermark: Union[Dict[PartitionLocator, int], int],
+        high_watermark: int,
         compacted_delta_locator: DeltaLocator,
         compacted_pyarrow_write_result: PyArrowWriteResult,
+        pk_index_pyarrow_write_result: PyArrowWriteResult,
         sort_keys_bit_width: int,
+        primary_key_index_version_locator: PrimaryKeyIndexVersionLocator,
         rebase_source_partition_locator: Optional[PartitionLocator],
     ) -> RoundCompletionInfo:
 
@@ -26,12 +26,14 @@ class RoundCompletionInfo(dict):
         rci["highWatermark"] = high_watermark
         rci["compactedDeltaLocator"] = compacted_delta_locator
         rci["compactedPyarrowWriteResult"] = compacted_pyarrow_write_result
+        rci["pkIndexPyarrowWriteResult"] = pk_index_pyarrow_write_result
         rci["sortKeysBitWidth"] = sort_keys_bit_width
+        rci["primaryKeyIndexVersionLocator"] = primary_key_index_version_locator
         rci["rebaseSourcePartitionLocator"] = rebase_source_partition_locator
         return rci
 
     @property
-    def high_watermark(self) -> Union[Dict[PartitionLocator, int], int]:
+    def high_watermark(self) -> int:
         return self["highWatermark"]
 
     @property
@@ -49,8 +51,24 @@ class RoundCompletionInfo(dict):
         return val
 
     @property
+    def pk_index_pyarrow_write_result(self) -> PyArrowWriteResult:
+        val: Dict[str, Any] = self.get("pkIndexPyarrowWriteResult")
+        if val is not None and not isinstance(val, PyArrowWriteResult):
+            self["pkIndexPyarrowWriteResult"] = val = PyArrowWriteResult(val)
+        return val
+
+    @property
     def sort_keys_bit_width(self) -> int:
         return self["sortKeysBitWidth"]
+
+    @property
+    def primary_key_index_version_locator(self) -> PrimaryKeyIndexVersionLocator:
+        val: Dict[str, Any] = self.get("primaryKeyIndexVersionLocator")
+        if val is not None and not isinstance(val, PrimaryKeyIndexVersionLocator):
+            self["primaryKeyIndexVersionLocator"] = val = PrimaryKeyIndexVersionLocator(
+                val
+            )
+        return val
 
     @property
     def rebase_source_partition_locator(self) -> Optional[PartitionLocator]:
