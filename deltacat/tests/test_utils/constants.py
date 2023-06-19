a84e96228551ff5e13e93770bf270eb5b33ab4ef
Raghavendra M Dani <draghave@amazon.com>
2023-06-19 12:13:04 -0700

    Setting hash bucket count based on POC results (#139)
    * Setting hash bucket count based on POC results and ensuring the deltas are correctly distributed across cluster
    
    [Description] This change ensures the hash bucket count is optimal, which is derived based on the earlier POC results. A ratio between pyarrow memory bytes and the optimal hash bucket count is introduced.
    
    [Motivation] #138
    
    * Fix module import in deltacat tests
    
    * fix file path
    
    * address comments

---
+++ deltacat/tests/test_utils/constants.py
@@ -0,0 +1,7 @@
+import json
+from deltacat.storage import Delta
+
+test_delta_file = open("deltacat/tests/test_utils/resources/test_delta.json")
+test_delta_dict = json.load(test_delta_file)
+
+TEST_DELTA = Delta(test_delta_dict)
