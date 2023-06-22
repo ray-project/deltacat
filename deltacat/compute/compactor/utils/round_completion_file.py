b9f76a6c17c06043c7fd4d24615bcb9db9899acf
Jialin Liu <rootliu@amazon.com>
2023-06-21 23:05:31 -0700

    remove repartition rcf write, generalize the rcf write by supporting pre-specified url

--- deltacat/compute/compactor/utils/round_completion_file.py
+++ deltacat/compute/compactor/utils/round_completion_file.py
@@ -5,6 +5,7 @@ from deltacat import logs
 from deltacat.compute.compactor import RoundCompletionInfo
 from deltacat.storage import PartitionLocator
 from deltacat.aws import s3u as s3_utils
+from typing import Optional
 
 logger = logs.configure_deltacat_logger(logging.getLogger(__name__))
 
@@ -36,40 +37,19 @@ def read_round_completion_file(
 
 
 def write_round_completion_file(
-    bucket: str,
-    source_partition_locator: PartitionLocator,
+    bucket: Optional[str],
+    source_partition_locator: Optional[PartitionLocator],
     round_completion_info: RoundCompletionInfo,
+    completion_file_s3_url: str = None,
 ) -> str:
 
     logger.info(f"writing round completion file contents: {round_completion_info}")
-    round_completion_file_s3_url = get_round_completion_file_s3_url(
-        bucket,
-        source_partition_locator,
-    )
-    logger.info(f"writing round completion file to: {round_completion_file_s3_url}")
-    s3_utils.upload(
-        round_completion_file_s3_url, str(json.dumps(round_completion_info))
-    )
-    logger.info(f"round completion file written to: {round_completion_file_s3_url}")
-    return round_completion_file_s3_url
-
-
-# write rcf given s3 url
-def write_repartition_completion_file(
-    repartition_completion_file_s3_url: str,
-    round_completion_info: RoundCompletionInfo,
-) -> str:
-
-    logger.info(
-        f"writing repartition completion file contents: {round_completion_info}"
-    )
-    logger.info(
-        f"writing repartition completion file to: {repartition_completion_file_s3_url}"
-    )
-    s3_utils.upload(
-        repartition_completion_file_s3_url, str(json.dumps(round_completion_info))
-    )
-    logger.info(
-        f"repartition completion file written to: {repartition_completion_file_s3_url}"
-    )
-    return repartition_completion_file_s3_url
+    if completion_file_s3_url is None:
+        completion_file_s3_url = get_round_completion_file_s3_url(
+            bucket,
+            source_partition_locator,
+        )
+    logger.info(f"writing round completion file to: {completion_file_s3_url}")
+    s3_utils.upload(completion_file_s3_url, str(json.dumps(round_completion_info)))
+    logger.info(f"round completion file written to: {completion_file_s3_url}")
+    return completion_file_s3_url
