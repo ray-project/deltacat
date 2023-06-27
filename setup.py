bc7c95586b41401f8d4f7478caebfb22528bc296
Rushan Jiang <rushanj@andrew.cmu.edu>
2023-06-27 14:44:05 -0700

    fix requirements list and setup
    Signed-off-by: Rushan Jiang <rushanj@andrew.cmu.edu>

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setuptools.setup(
         "pandas == 1.3.5",
         "pyarrow == 10.0.1",
         "pydantic == 1.10.4",
+        "pyiceberg @ git+https://github.com/apache/iceberg#subdirectory=python",
         "ray[default] ~= 2.0",
         "s3fs == 2023.1.0",
         "tenacity == 8.1.0",
