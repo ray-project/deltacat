33c41df62073bc34a95a36a67ecc39aabdc5fd3e
Raghavendra Dani <draghave@amazon.com>
2023-07-24 20:19:58 -0700

    Bunping up version to 0.1.18b8

--- deltacat/__init__.py
+++ deltacat/__init__.py
@@ -43,7 +43,7 @@ from deltacat.types.tables import TableWriteMode
 
 deltacat.logs.configure_deltacat_logger(logging.getLogger(__name__))
 
-__version__ = "0.1.18b7"
+__version__ = "0.1.18b8"
 
 
 __all__ = [
