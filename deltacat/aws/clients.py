b464c8cbe65a82be898ff6a7dee8032e4c6b5893
Raghavendra Dani <draghave@amazon.com>
2023-07-24 20:40:23 -0700

    Use kwargs as to determine the profile to use when creating the s3 client

--- deltacat/aws/clients.py
+++ deltacat/aws/clients.py
@@ -13,10 +13,25 @@ from deltacat.aws.constants import BOTO_MAX_RETRIES
 
 logger = logs.configure_deltacat_logger(logging.getLogger(__name__))
 
+BOTO3_PROFILE_NAME_KWARG_KEY = "boto3_profile_name"
+
+
+def _get_session_from_kwargs(input_kwargs):
+    if input_kwargs.get(BOTO3_PROFILE_NAME_KWARG_KEY) is not None:
+        boto3_session = boto3.Session(
+            profile_name=input_kwargs.get(BOTO3_PROFILE_NAME_KWARG_KEY)
+        )
+        input_kwargs.pop(BOTO3_PROFILE_NAME_KWARG_KEY)
+        return boto3_session
+    else:
+        return boto3.Session()
+
 
 def _resource(name: str, region: Optional[str], **kwargs) -> ServiceResource:
+    boto3_session = _get_session_from_kwargs(kwargs)
+
     boto_config = Config(retries={"max_attempts": BOTO_MAX_RETRIES, "mode": "standard"})
-    return boto3.resource(
+    return boto3_session.resource(
         name,
         region,
         config=boto_config,
@@ -30,10 +45,11 @@ def _client(name: str, region: Optional[str], **kwargs) -> BaseClient:
         return resource_cache(name, region, **kwargs).meta.client
     except ResourceNotExistsError:
         # fall back for clients without an associated resource
+        boto3_session = _get_session_from_kwargs(kwargs)
         boto_config = Config(
             retries={"max_attempts": BOTO_MAX_RETRIES, "mode": "standard"}
         )
-        return boto3.client(
+        return boto3_session.client(
             name,
             region,
             config=boto_config,
