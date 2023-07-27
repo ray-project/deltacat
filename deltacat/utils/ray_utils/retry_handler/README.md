360aaae62b1d7ee1e8e45d1fa644e96a21310007
Ekas Chawla <ekaschaw@amazon.com>
2023-07-27 11:51:40 -0700

    updated README and implemented AIMDBatchScaling

--- deltacat/utils/ray_utils/retry_handler/README.md
+++ deltacat/utils/ray_utils/retry_handler/README.md
@@ -10,12 +10,12 @@ Params:
 
 Use cases:
 1. Notifying progress
-    This will be done through ProgressNotifierInterface. The client can use has_progress and send_progress
+    This will be done through ProgressNotifierInterface. The client can implement has_progress and send_progress from the interface
     to recieve updates on task level progress. This can be an SNSQueue or any type of indicator the client may choose.
 2. Detecting stragglers
    Given the straggler detection algorithm implemented by StragglerDetectionInterface, the method is_straggler will inform 
-   the customer if the current node is a straggler according to their own logic and proving them with TaskContext, the information 
-   they might need to make that decision.
+   the customer if the current node is a straggler according to their own logic. In order to make their decision, we will provide them 
+   with TaskContext that contains fields and data that the client can use to decide if a task is a straggler or not.
 3. Retrying retryable exceptions
    Within the failure directory, there are common errors that are retryable and when detected as an instance 
    of the retryable class, will cause the task to be retried when the exception is caught. If the client would like
--- deltacat/utils/ray_utils/retry_handler/ray_remote_tasks_batch_scaling_strategy.py
+++ deltacat/utils/ray_utils/retry_handler/ray_remote_tasks_batch_scaling_strategy.py
@@ -1,24 +1,42 @@
+from typing import List, Any
 from deltacat.utils.ray_utils.retry_handler.batch_scaling_interface import BatchScalingInterface
 class RayRemoteTasksBatchScalingStrategy(BatchScalingInterface):
     """
     Default batch scaling parameters for if the client does not provide their own batch_scaling parameters
     """
-
-    def init_tasks(self)-> None:
-            """
-            Setup AIMD scaling for the batches as the default
-            """
-        pass
-
-    def next_batch(self) -> List:
+    def __init__(self, additive_increase: int, multiplicative_decrease: float):
+        self.task_infos = []
+        self.batch_index = 0
+        self.batch_size = None
+        self.max_batch_size = None
+        self.min_batch_size = None
+        self.additive_increase = additive_increase
+        self.multiplicative_decrease = multiplicative_decrease
+    def init_tasks(self, initial_batch_size: int, max_batch_size: int, min_batch_size: int, task_infos: List[TaskInfoObject])-> None:
         """
-        Returns the list of tasks included in the next batch of whatever size based on AIMD
+        Setup AIMD scaling for the batches as the default
         """
+        self.task_infos = task_infos
+        self.batch_size = initial_batch_size
+        self.max_batch_size = max_batch_size
+        self.min_batch_size = min_batch_size
 
-        pass
 
     def has_next_batch(self) -> bool:
         """
+        Returns the list of tasks included in the next batch of whatever size based on AIMD
+        """
+        return self.batch_index < len(self.task_infos)
+
+
+    def next_batch(self) -> List[TaskInfoObject]:
+        """
         If there are no more tasks to execute that can not create a batch, return False
         """
-        pass
+        batch_end = min(self.batch_index + self.batch_size, len(self.task_infos))
+        batch = self.task_infos[self.batch_index:batch_end]
+        self.batch_index = batch_end
+        return batch
+
+    def mark_task_completed(self, task_info: TaskInfoObject) -> None:
+
