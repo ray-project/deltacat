a78cc4fb413a1c102afdb5bfe5894541c4cbb683
Pierce Faraone <faraonep@amazon.com>
2023-08-02 11:16:57 -0700

    block_until_instance_metadata_service_returns_success implemented

--- deltacat/aws/clients.py
+++ deltacat/aws/clients.py
@@ -1,12 +1,15 @@
 import logging
 from functools import lru_cache
+import requests
 from typing import Optional
+from http import HTTPStatus
 
 import boto3
 from boto3.exceptions import ResourceNotExistsError
 from boto3.resources.base import ServiceResource
 from botocore.client import BaseClient
 from botocore.config import Config
+from requests.adapters import HTTPAdapter, Retry, Response
 
 from deltacat import logs
 from deltacat.aws.constants import BOTO_MAX_RETRIES
@@ -14,9 +17,45 @@ from deltacat.aws.constants import BOTO_MAX_RETRIES
 logger = logs.configure_deltacat_logger(logging.getLogger(__name__))
 
 BOTO3_PROFILE_NAME_KWARG_KEY = "boto3_profile_name"
+INSTANCE_METADATA_SERVICE_URI_BASE = "169.254.169.254/latest/meta-data"
+INSTANCE_PROFILE_ROLE_CREDENTIALS_PATH = (
+    "/identity-credentials/ec2/security-credentials/ec2-instance"
+)
+
+
+def block_until_instance_metadata_service_returns_success(
+    total_number_of_retries=10,
+    backoff_factor=1,
+) -> Optional[Response]:
+    # https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instancedata-data-retrieval.html
+    with requests.Session as session:
+        retries = Retry(
+            total=total_number_of_retries,
+            backoff_factor=backoff_factor,  # A backoff factor to apply between attempts after the second try: {backoff factor} * (2 ** ({number of previous retries}))
+            # For example, if the backoff_factor is 1, then Retry.sleep() will sleep for [1s, 2s, 4s, 8s, …] between retries.
+            status_forcelist=[
+                HTTPStatus.TOO_MANY_REQUESTS,
+                HTTPStatus.INTERNAL_SERVER_ERROR,
+                HTTPStatus.BAD_GATEWAY,
+                HTTPStatus.SERVICE_UNAVAILABLE,
+                HTTPStatus.GATEWAY_TIMEOUT,
+            ],
+            raise_on_status=True,  # Whether, if the number of retries are exhausted, to raise a MaxRetryError, or to return a response with a response code in the 3xx range.
+        )
+        adapter = HTTPAdapter(max_retries=retries)
+        session.mount("http", adapter)
+        response = session.get(
+            f"http://{INSTANCE_METADATA_SERVICE_URI_BASE}{INSTANCE_PROFILE_ROLE_CREDENTIALS_PATH}"
+        )
+        print(f"response={response.text}")
+        logger.info(
+            f"Instance profile credentials are now accessible. Instance Metadata Service (IMDS) returned success with status code {response.status_code}"
+        )
+        return response
 
 
 def _get_session_from_kwargs(input_kwargs):
+    block_until_instance_metadata_service_returns_success()
     if input_kwargs.get(BOTO3_PROFILE_NAME_KWARG_KEY) is not None:
         boto3_session = boto3.Session(
             profile_name=input_kwargs.get(BOTO3_PROFILE_NAME_KWARG_KEY)
@@ -67,3 +106,6 @@ def client_cache(name: str, region: Optional[str], **kwargs) -> BaseClient:
     # we don't use the @lru_cache decorator because Ray can't pickle it
     cached_function = lru_cache()(_client)
     return cached_function(name, region, **kwargs)
+
+
+# print(block_until_instance_metadata_service_returns_success())
