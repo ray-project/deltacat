69b4d884b25f5c7f837a964dd28f8f3867c5d1a9
pf <19919899+pfaraone@users.noreply.github.com>
2023-08-07 16:58:54 -0700

    version bump from 0.1.18b13 to 0.1.18b14 (#179)

--- deltacat/__init__.py
+++ deltacat/__init__.py
@@ -44,7 +44,7 @@ from deltacat.types.tables import TableWriteMode
 
 deltacat.logs.configure_deltacat_logger(logging.getLogger(__name__))
 
-__version__ = "0.1.18b13"
+__version__ = "0.1.18b14"
 
 
 __all__ = [
