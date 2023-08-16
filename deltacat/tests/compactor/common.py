be43419a6c3283a459c449cb742b86d2ea2bdc52
Pierce Faraone <faraonep@amazon.com>
2023-08-15 17:25:50 -0700

    added incremental timestamp-pk unit test

--- deltacat/tests/compactor/common.py
+++ deltacat/tests/compactor/common.py
@@ -1,6 +1,9 @@
 # Allow classes to use self-referencing Type hints in Python 3.7.
 from __future__ import annotations
 from enum import Enum
+from typing import List
+import datetime as dt
+from datetime import timezone
 
 TEST_S3_RCF_BUCKET_NAME = "test-compaction-artifacts-bucket"
 # REBASE  src = spark compacted table to create an initial version of ray compacted table
@@ -13,6 +16,8 @@ BASE_TEST_DESTINATION_TABLE_NAME = "destination_test_table_RAY"
 BASE_TEST_DESTINATION_TABLE_VERSION = "1"
 
 MAX_RECORDS_PER_FILE: int = 1
+
+UTC_ISO_8601_FORMAT_WITHOUT_MILLIS = "%Y-%m-%dT%H:%M:%SZ"  # '2018-09-05T14:09:03Z'
 
 
 class PartitionKeyType(str, Enum):
@@ -50,3 +55,26 @@ def setup_sort_and_partition_keys(sort_keys_param, partition_keys_param):
             for partition_key in partition_keys_param
         ]
     return sort_keys, partition_keys
+
+
+def offer_iso8601_timestamp_list(
+    periods: int,
+    unit_of_time: str,
+    start_time=dt.datetime(2023, 5, 3, 10, 0, 0, 0, tzinfo=timezone.utc),
+) -> List[str]:
+    import datetime as dt
+
+    acceptable_units_of_time = ["seconds", "minutes", "hours", "days", "weeks"]
+    if unit_of_time not in acceptable_units_of_time:
+        raise ValueError(
+            f"unit_of_time {unit_of_time} is not supported. Please use one of these time units: {acceptable_units_of_time}"
+        )
+    res = []
+    for i in range(periods):
+        kwarg = {unit_of_time: i}
+        res.append(
+            (start_time - dt.timedelta(**kwarg)).strftime(
+                UTC_ISO_8601_FORMAT_WITHOUT_MILLIS
+            )
+        )
+    return res
--- deltacat/tests/compactor/testcases.py
+++ deltacat/tests/compactor/testcases.py
@@ -3,8 +3,8 @@ from __future__ import annotations
 import pyarrow as pa
 from deltacat.tests.compactor.common import (
     MAX_RECORDS_PER_FILE,
+    offer_iso8601_timestamp_list,
 )
-
 
 INCREMENTAL_TEST_CASES = {
     "1-incremental-pkstr-sknone-norcf": (
@@ -150,6 +150,67 @@ INCREMENTAL_TEST_CASES = {
         MAX_RECORDS_PER_FILE,
         None,
     ),
+    "7-incremental-integer-pk": (
+        ["pk_col_1"],
+        [
+            {
+                "key_name": "sk_col_1",
+            },
+        ],
+        [],
+        ["pk_col_1", "sk_col_1"],
+        [
+            pa.array([i for i in range(0, 10)]),
+            pa.array([str(i) for i in range(10)]),
+        ],
+        None,
+        ["1"],
+        pa.Table.from_arrays(
+            [
+                pa.array([i for i in range(0, 10)]),
+                pa.array([str(i) for i in range(10)]),
+            ],
+            names=["pk_col_1", "sk_col_1"],
+        ),
+        None,
+        None,
+        True,
+        False,
+        True,
+        MAX_RECORDS_PER_FILE,
+        None,
+    ),
+    "8-incremental-timestamp-pk": (
+        ["pk_col_1"],
+        [
+            {
+                "key_name": "sk_col_1",
+            },
+        ],
+        [],
+        ["pk_col_1", "sk_col_1"],
+        [
+            pa.array(offer_iso8601_timestamp_list(10, "minutes")),
+            pa.array([str(i) for i in range(10)]),
+        ],
+        None,
+        ["1"],
+        pa.Table.from_arrays(
+            [
+                pa.array(offer_iso8601_timestamp_list(10, "minutes")),
+                pa.array([str(i) for i in range(10)]),
+            ],
+            names=["pk_col_1", "sk_col_1"],
+        ),
+        None,
+        None,
+        True,
+        False,
+        True,
+        MAX_RECORDS_PER_FILE,
+        None,
+    ),
+    # yyyy-MM-dd HH:mm:ssZZ
     # decimal, int, timestamp pk, multipk
     # honoring sort key
 }
