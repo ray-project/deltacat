7e90c945fa6b980b42595d152ba2d4d702033337
Sammy Sidhu <samster25@users.noreply.github.com>
2023-08-22 20:32:55 -0700

    add schema override unit test

--- deltacat/tests/utils/test_daft.py
+++ deltacat/tests/utils/test_daft.py
@@ -2,7 +2,9 @@ import unittest
 from deltacat.types.media import ContentEncoding, ContentType
 from deltacat.utils.daft import daft_s3_file_to_table
 
+from deltacat.utils.pyarrow import ReadKwargsProviderPyArrowSchemaOverride
 
+import pyarrow as pa
 class TestDaftParquetReader(unittest.TestCase):
     MVP_PATH = "deltacat/tests/utils/data/mvp.parquet"
 
@@ -24,6 +26,20 @@ class TestDaftParquetReader(unittest.TestCase):
         )
         self.assertEqual(table.schema.names, ["b"])
         self.assertEqual(table.num_rows, 100)
+
+    def test_read_from_s3_single_column_with_schema(self):
+        schema = pa.schema([('a', pa.int64()), ('b', pa.string())])
+        pa_read_func_kwargs_provider = ReadKwargsProviderPyArrowSchemaOverride(schema = schema)
+        table = daft_s3_file_to_table(
+            self.MVP_PATH,
+            content_encoding=ContentEncoding.IDENTITY.value,
+            content_type=ContentType.PARQUET.value,
+            include_columns=["b"],
+            pa_read_func_kwargs_provider=pa_read_func_kwargs_provider
+        )
+        self.assertEqual(table.schema.names, ["b"])
+        self.assertEqual(table.num_rows, 100)
+
 
 
 if __name__ == "__main__":
