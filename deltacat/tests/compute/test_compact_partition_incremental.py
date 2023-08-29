3648b1e8da75c23e340835685c732b541410646d
Pierce Faraone <faraonep@amazon.com>
2023-08-28 17:48:25 -0700

    moved ray.shutdown() to before ray.init

--- deltacat/tests/compute/test_compact_partition_incremental.py
+++ deltacat/tests/compute/test_compact_partition_incremental.py
@@ -66,10 +66,10 @@ def setup_compaction_artifacts_s3_bucket(setup_s3_resource: ServiceResource):
 def setup_ray_cluster():
     # module scoped starting up a ray cluster as it can be shared between parametrized test functions without side effects
     # calling ray.shutdown() ensures that any other ray instance started up by other test suites will not interfere with this one
+    ray.shutdown()
     ray.init(local_mode=True)
     assert ray.is_initialized()
     yield
-    ray.shutdown()
 
 
 """
