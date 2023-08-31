f2f74e735059781d4de1db7b3ab49e9bc951d5d0
Kevin Yan <kevnya@amazon.com>
2023-08-31 16:57:07 +0000

    Remove hardcoded regional endpoint in favor of dynamic inferral

--- deltacat/aws/constants.py
+++ deltacat/aws/constants.py
@@ -5,6 +5,3 @@ from deltacat.utils.common import env_integer, env_string
 BOTO_MAX_RETRIES = env_integer("BOTO_MAX_RETRIES", 15)
 TIMEOUT_ERROR_CODES: List[str] = ["ReadTimeoutError", "ConnectTimeoutError"]
 AWS_REGION = env_string("AWS_REGION", "us-east-1")
-STS_REGIONAL_ENDPOINT = env_string(
-    "AWS_ENDPOINT_URL_STS", "https://sts.us-east-1.amazonaws.com"
-)
--- deltacat/compute/metastats/stats.py
+++ deltacat/compute/metastats/stats.py
@@ -8,7 +8,7 @@ from ray.types import ObjectRef
 from deltacat import logs
 from deltacat.aws import s3u as s3_utils
 from deltacat.aws.clients import client_cache
-from deltacat.aws.constants import AWS_REGION, AWS_ENDPOINT_URL_STS
+from deltacat.aws.constants import AWS_REGION
 from deltacat.compute.compactor import DeltaAnnotated
 from deltacat.compute.metastats.utils.io import (
     cache_inflation_rate_data_for_delta_stats_ready,
@@ -95,11 +95,7 @@ def start_stats_collection(
 
 
 def _get_account_id() -> str:
-    client = client_cache(
-        "sts",
-        region_name=AWS_REGION,
-        endpoint_url=AWS_ENDPOINT_URL_STS,
-    )
+    client = client_cache("sts", region_name=AWS_REGION)
     account_id = client.get_caller_identity()["Account"]
     return account_id
 
