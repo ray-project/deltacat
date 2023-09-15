14dc7722616fb996755efec2fa9a9f9d9a9450d5
Kevin Yan <kevnya@amazon.com>
2023-09-15 23:51:37 +0000

    Derive optional metric configurations from metrics_config
    The current implementation explicitly sets metric dimensions and
    namespace. These configurations are not necessarily desired by
    everybody, and thus should be derived from the passed-in metrics_config
    object instead. This change will "break" existing metrics, thus
    necessitating a minor package version bump.

--- deltacat/__init__.py
+++ deltacat/__init__.py
@@ -44,7 +44,7 @@ from deltacat.types.tables import TableWriteMode
 
 deltacat.logs.configure_deltacat_logger(logging.getLogger(__name__))
 
-__version__ = "0.1.20"
+__version__ = "0.2.0"
 
 
 __all__ = [
