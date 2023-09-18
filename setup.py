08372a605b3be016488a41bbe706f43638b525e1
Pierce Faraone <faraonep@amazon.com>
2023-09-19 04:24:38 +0800

    version bump s3fs to match aiobotocore==2.5.0

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setuptools.setup(
         "pyarrow == 12.0.1",
         "pydantic == 1.10.4",
         "ray[default] ~= 2.0",
-        "s3fs == 2022.2.0",
+        "s3fs == 2023.6.0",
         "tenacity == 8.1.0",
         "typing-extensions == 4.4.0",
         "pymemcache == 4.0.0",
