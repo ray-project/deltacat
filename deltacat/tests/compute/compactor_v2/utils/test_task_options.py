626f806579c97b3abb1597b275ad2f692b6d44a4
Raghavendra Dani <draghave@amazon.com>
2023-09-29 17:05:07 -0700

    Add retries and retry_exceptions to overcome throttling

---
+++ deltacat/tests/compute/compactor_v2/utils/test_task_options.py
@@ -0,0 +1,33 @@
+import unittest
+import ray
+from deltacat.compute.compactor_v2.utils.task_options import get_task_options
+
+
+@ray.remote
+def valid_func():
+    return 2
+
+
+@ray.remote
+def throwing_func():
+    raise ConnectionAbortedError()
+
+
+class TestTaskOptions(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        ray.init(local_mode=True, ignore_reinit_error=True)
+        super().setUpClass()
+
+    def test_get_task_options_sanity(self):
+        opts = get_task_options(0.01, 0.01)
+        result_ref = valid_func.options(**opts).remote()
+        result = ray.get(result_ref)
+
+        self.assertEqual(result, 2)
+
+    def test_get_task_options_when_exception_is_thrown(self):
+        opts = get_task_options(0.01, 0.01)
+        result_ref = throwing_func.options(**opts).remote()
+
+        self.assertRaises(ConnectionAbortedError, lambda: ray.get(result_ref))
