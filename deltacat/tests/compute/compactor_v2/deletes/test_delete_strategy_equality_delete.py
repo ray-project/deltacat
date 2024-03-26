6b85e29580793f72d512bf61db52a2e892b2daa2
Pierce Faraone <faraonep@amazon.com>
2024-03-26 17:27:02 +0000

    added single test cases for TestEqualityDeleteStrategy

--- deltacat/tests/compute/compactor_v2/deletes/test_delete_strategy_equality_delete.py
+++ deltacat/tests/compute/compactor_v2/deletes/test_delete_strategy_equality_delete.py
@@ -2,70 +2,136 @@ import pytest
 
 from deltacat.storage import (
     DeltaType,
-    Delta,
 )
-from deltacat.storage import (
-    Partition,
-    PartitionLocator,
-    Stream,
+from deltacat.compute.compactor_v2.deletes.delete_file_envelope import (
+    DeleteFileEnvelope,
 )
-from deltacat.tests.compute.test_util_constant import (
-    TEST_S3_RCF_BUCKET_NAME,
-)
-from deltacat.tests.compute.test_util_common import (
-    create_src_table,
-    create_destination_table,
-)
-
 from dataclasses import dataclass, fields
 import ray
-import os
-from typing import Any, Dict, List, Optional, Tuple
-import deltacat.tests.local_deltacat_storage as ds
-from deltacat.compute.compactor.model.compact_partition_params import (
-    CompactPartitionParams,
-)
+from typing import List
 import pyarrow as pa
-from deltacat.storage import DeleteParameters
-
-
-DATABASE_FILE_PATH_KEY, DATABASE_FILE_PATH_VALUE = (
-    "db_file_path",
-    "deltacat/tests/local_deltacat_storage/db_test.sqlite",
-)
 
 
 @dataclass(frozen=True)
-class PrepareDeleteTestCaseParams:
+class ApplyAllDeletesTestCaseParams:
     """
-    A pytest parameterized test case for the `prepare_deletes` function.
+    A pytest parameterized test case for the `apply_all_deletes` function.
     """
 
-    deltas_to_compact: List[Tuple[pa.Table, DeltaType, Optional[DeleteParameters]]]
-    expected_delta_file_envelopes_len: int
-    expected_delete_table: List[pa.Table]
-    expected_non_delete_deltas_length: int
-    throws_error_type: BaseException
+    table: pa.Table
+    delete_file_envelopes: List[DeleteFileEnvelope]
+    expected_table: pa.Table
+    expected_total_dropped_rows: int
+    expected_exception: BaseException
 
     # makes TestCaseParams iterable which is required to build the list of pytest.param values to pass to pytest.mark.parametrize
     def __iter__(self):
         return (getattr(self, field.name) for field in fields(self))
 
 
-@pytest.fixture(scope="function")
-def local_deltacat_storage_kwargs(request: pytest.FixtureRequest):
-    # see deltacat/tests/local_deltacat_storage/README.md for documentation
-    kwargs_for_local_deltacat_storage: Dict[str, Any] = {
-        DATABASE_FILE_PATH_KEY: DATABASE_FILE_PATH_VALUE,
-    }
-    yield kwargs_for_local_deltacat_storage
-    if os.path.exists(DATABASE_FILE_PATH_VALUE):
-        os.remove(DATABASE_FILE_PATH_VALUE)
+TEST_CASES_APPLY_MANY_DELETES = {
+    "1-test-sanity": ApplyAllDeletesTestCaseParams(
+        table=pa.Table.from_arrays(
+            [
+                pa.array([0, 1]),
+                pa.array(["0", "1"]),
+            ],
+            names=["pk_col_1", "col_1"],
+        ),
+        delete_file_envelopes=[
+            DeleteFileEnvelope.of(
+                1,
+                DeltaType.DELETE,
+                pa.Table.from_arrays(
+                    [
+                        pa.array(["1"]),
+                    ],
+                    names=["col_1"],
+                ),
+                ["col_1"],
+            )
+        ],
+        expected_table=pa.Table.from_arrays(
+            [
+                pa.array(
+                    [
+                        0,
+                    ]
+                ),
+                pa.array(["0"]),
+            ],
+            names=["pk_col_1", "col_1"],
+        ),
+        expected_total_dropped_rows=1,
+        expected_exception=None,
+    ),
+}
 
 
 class TestEqualityDeleteStrategy:
     def test_apply_deletes(self):
+        # TODO:
         pass
 
-    def test_apply_all_deletes(self):
-        pass
+    @pytest.mark.parametrize(
+        [
+            "test_name",
+            "table",
+            "delete_file_envelopes",
+            "expected_table",
+            "expected_total_dropped_rows",
+            "expected_exception",
+        ],
+        [
+            (
+                test_name,
+                table,
+                delete_file_envelopes,
+                expected_table,
+                expected_total_dropped_rows,
+                expected_exception,
+            )
+            for test_name, (
+                table,
+                delete_file_envelopes,
+                expected_table,
+                expected_total_dropped_rows,
+                expected_exception,
+            ) in TEST_CASES_APPLY_MANY_DELETES.items()
+        ],
+        ids=[test_name for test_name in TEST_CASES_APPLY_MANY_DELETES],
+    )
+    def test_apply_all_deletes(
+        self,
+        test_name: str,
+        table: pa.Table,
+        delete_file_envelopes: List[DeleteFileEnvelope],
+        expected_table: pa.Table,
+        expected_total_dropped_rows: int,
+        expected_exception: BaseException,
+    ):
+        from deltacat.compute.compactor_v2.deletes.delete_strategy_equality_delete import (
+            EqualityDeleteStrategy,
+        )
+        from deltacat.compute.compactor_v2.deletes.delete_strategy import (
+            DeleteStrategy,
+        )
+
+        delete_strategy: DeleteStrategy = EqualityDeleteStrategy()
+        ray.shutdown()
+        ray.init(local_mode=True, ignore_reinit_error=True)
+        if expected_exception:
+            with pytest.raises(expected_exception):
+                delete_strategy.apply_all_deletes(
+                    table,
+                    delete_file_envelopes,
+                )
+            return
+        actual_table, actual_dropped_rows = delete_strategy.apply_all_deletes(
+            table,
+            delete_file_envelopes,
+        )
+        assert expected_table.combine_chunks().equals(
+            actual_table
+        ), f"{expected_table} does not match {actual_table}"
+        assert expected_total_dropped_rows == actual_dropped_rows
