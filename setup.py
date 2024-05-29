382b578aacfcf23c8039f6e66b097768b0af428b
Dmitri Gekhtman <gekhtman@amazon.com>
2024-05-29 03:01:52 +0000

    Update deps

--- setup.py
+++ setup.py
@@ -36,13 +36,13 @@ setuptools.setup(
     install_requires=[
         # any changes here should also be reflected in requirements.txt
         "aws-embedded-metrics == 3.2.0",
-        "boto3 ~= 1.20",
+        "boto3 ~= 1.34",
         "numpy == 1.21.5",
         "pandas == 1.3.5",
         "pyarrow == 12.0.1",
         "pydantic == 1.10.4",
         "ray[default] ~= 2.0",
-        "s3fs == 2022.2.0",
+        "s3fs == 2024.5.0",
         "tenacity == 8.1.0",
         "typing-extensions == 4.4.0",
         "pymemcache == 4.0.0",
