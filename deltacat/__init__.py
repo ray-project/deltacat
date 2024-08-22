be59884090b17d044f63552a5afac58e85165fa5
Akindu Dasanayake <akindu@amazon.com>
2024-08-22 18:07:43 +0000

    Deltacat version bump to 1.1.15

--- deltacat/__init__.py
+++ deltacat/__init__.py
@@ -44,7 +44,7 @@ from deltacat.types.tables import TableWriteMode
 
 deltacat.logs.configure_deltacat_logger(logging.getLogger(__name__))
 
-__version__ = "1.1.14"
+__version__ = "1.1.15"
 
 
 __all__ = [
