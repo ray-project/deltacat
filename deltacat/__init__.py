ebbd210d0fe0a35031eec7b90a3a7282a4112cf6
Raghavendra M Dani <draghave@amazon.com>
2024-09-03 12:50:13 -0700

    Bump version (#351)

--- deltacat/__init__.py
+++ deltacat/__init__.py
@@ -44,7 +44,7 @@ from deltacat.types.tables import TableWriteMode
 
 deltacat.logs.configure_deltacat_logger(logging.getLogger(__name__))
 
-__version__ = "1.1.16"
+__version__ = "1.1.17"
 
 
 __all__ = [
