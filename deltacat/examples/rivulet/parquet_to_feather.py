5e262a0b720cf84b18365d1cb6c6deb01ba51dbf
Anshuman Komawar <anshumankomawar@gmail.com>
2025-01-10 18:26:32 -0800

    fix: add configurable args, improve code readability

--- deltacat/examples/rivulet/parquet_to_feather.py
+++ deltacat/examples/rivulet/parquet_to_feather.py
@@ -1,81 +1,99 @@
+import os
+import deltacat as dc
+
 import pyarrow as pa
 import pyarrow.parquet as pq
-import deltacat as dc
-from deltacat.storage.rivulet.reader.query_expression import QueryExpression
 
-# Step 1: Create a simple 3x3 Parquet file using pyarrow
-parquet_file_path = "../contacts.parquet"
-data = {
-    "id": [1, 2, 3],
-    "name": ["Alice", "Bob", "Charlie"],
-    "age": [25, 30, 35]
-}
-table = pa.Table.from_pydict(data)
-pq.write_table(table, parquet_file_path)
-
-# Step 2: Load the Parquet file into a Dataset
-dataset = dc.Dataset.from_parquet(
-    name="contacts",
-    file_uri=parquet_file_path,
-    metadata_uri=".",
-    merge_keys="id"
+from deltacat.examples.common.fixtures import (
+    store_cli_args_in_os_environ,
 )
 
-# Step 3: Add two new columns to the Dataset
-dataset.add_fields([
-    ("email", dc.Datatype.string()),
-    ("is_active", dc.Datatype.bool())
-])
+"""
+This demo showcases
+1. How to create a dataset from a Parquet file using Deltacat.
+2. How to dynamically modify a dataset schema by adding new columns.
+3. How to append new rows and update existing rows without altering the original data files.
+4. How to query and read data from the updated dataset efficiently.
+"""
 
-# Step 4: Append two new records, including values for the new columns. The cool thing with deltacat datasets is
-#         that deltacat will not attempt to rewrite the existing parquet file, they will store additional data files
-#         alongside the original parquet file(s) that can be easily joined w/ the originals.
-
-# Open a new writer that will write new data to feather files
-dataset_writer = dataset.writer(file_format="feather")
-
-# Define some new rows w/ the expanded schema and write them
-new_rows = [
-    {
-        "id": 4,
-        "name": "David",
-        "age": 40,
-        "email": "david@example.com",
-        "is_active": True
-    },
-    {
-        "id": 5,
-        "name": "Eve",
-        "age": 45,
-        "email": "eve@example.com",
-        "is_active": False
+def run(**kwargs):
+    # Step 1: Create a simple 3x3 Parquet file using pyarrow
+    parquet_file_path = "./contacts.parquet"
+    data = {
+        "id": [1, 2, 3],
+        "name": ["Alice", "Bob", "Charlie"],
+        "age": [25, 30, 35]
     }
-]
-dataset_writer.write(new_rows)
+    table = pa.Table.from_pydict(data)
+    pq.write_table(table, parquet_file_path)
 
-# Write into the new columns on existing rows and write them, again without modifying/messing with the original parquet file.
-new_columns_existing_rows = [
-    {
-        "id": 3,
-        "email": "charlie@example.com",
-        "is_active": True
-    },
-    {
-        "id": 2,
-        "email": "bob@example.com",
-        "is_active": False
-    },
-    {
-        "id": 1,
-        "email": "alice@example.com",
-        "is_active": False
-    }
-]
-dataset_writer.write(new_columns_existing_rows)
+    # Step 2: Load the Parquet file into a Dataset
+    dataset = dc.Dataset.from_parquet(
+        name="contacts",
+        file_uri=parquet_file_path,
+        metadata_uri=".",
+        merge_keys="id"
+    )
 
-# Write dataset data/metadata into feather files.
-dataset_writer.flush()
+    # Step 3: Add two new columns to the Dataset
+    dataset.add_fields([
+        ("email", dc.Datatype.string()),
+        ("is_active", dc.Datatype.bool())
+    ])
 
-# Step 5: Read data from feather file.
-read_records = list(dataset.scan(QueryExpression()).to_arrow())
-[print(record) for record in read_records]
+    # Step 4: Append two new records, including values for the new columns. The cool thing with deltacat datasets is
+    #         that deltacat will not attempt to rewrite the existing parquet file, they will store additional data files
+    #         alongside the original parquet file(s) that can be easily joined w/ the originals.
+
+    # Open a new writer that will write new data to feather files
+    dataset_writer = dataset.writer(file_format="feather")
+
+    # Define some new rows w/ the expanded schema and write them
+    new_rows = [
+        {
+            "id": 4,
+            "name": "David",
+            "age": 40,
+            "email": "david@example.com",
+            "is_active": True
+        },
+        {
+            "id": 5,
+            "name": "Eve",
+            "age": 45,
+            "email": "eve@example.com",
+            "is_active": False
+        }
+    ]
+    dataset_writer.write(new_rows)
+
+    # Write into the new columns on existing rows and write them, again without modifying/messing with the original parquet file.
+    new_columns_existing_rows = [
+        {
+            "id": 3,
+            "email": "charlie@example.com",
+            "is_active": True
+        },
+        {
+            "id": 2,
+            "email": "bob@example.com",
+            "is_active": False
+        },
+        {
+            "id": 1,
+            "email": "alice@example.com",
+            "is_active": False
+        }
+    ]
+    dataset_writer.write(new_columns_existing_rows)
+
+    # Write dataset data/metadata into feather files.
+    dataset_writer.flush()
+
+    # Step 5: Read data from feather file.
+    for record in dataset.scan().to_arrow():
+        print(record)
+
+if __name__ == "__main__":
+    store_cli_args_in_os_environ()
+    run(**os.environ)
