4a740b276686cb83b7d17acdcda192217d6bf83d
Matt Cember <mccember@amazon.com>
2025-02-28 12:29:59 -0500

    small tweaks

--- deltacat/catalog/__init__.py
+++ deltacat/catalog/__init__.py
@@ -1,4 +1,3 @@
-# noqa: F401
 from deltacat.catalog.catalog_properties import (  # noqa: F401
     CatalogProperties,
     get_catalog_properties,
--- deltacat/storage/model/constants.py
+++ deltacat/storage/model/constants.py
@@ -1,11 +1,8 @@
 from deltacat.storage import StreamFormat
 
 # These are the default hardcoded values for Rivulet-DeltaCAT storage catalog.
-# TODO: Integrate these values into the storage interface dynamically.
-#       Currently, these are defined as static defaults, but they should
-#       be determined at runtime based on the storage catalog configuration.
-#       This will ensure that they remain consistent across different storage
-#       implementations and can be easily modified or overridden when needed.
+# These defaults should be applied in catalog interface implementations
+# Storage interface implementations should be agnostic to defaults and require full information
 DEFAULT_NAMESPACE = "DEFAULT"
 DEFAULT_TABLE_VERSION = "1"
 DEFAULT_STREAM_ID = "stream"
