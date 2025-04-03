7c8f88080213a8da17f984c4ae74ebae80a25709
valerie <valerie_eng@berkeley.edu>
2025-04-03 14:23:27 -0700

    fixed error with reading tar files and added sample tar files

--- deltacat/tests/storage/rivulet/schema/test_wds.py
+++ deltacat/tests/storage/rivulet/schema/test_wds.py
@@ -1,3 +1,4 @@
+import itertools
 import pytest
 import pyarrow as pa
 import json
@@ -5,7 +6,7 @@ import tarfile
 from deltacat.storage.rivulet import Dataset, Schema, Field, Datatype
 
 
-def test_schema_values():
+def test_schema_field_types():
     fields = [
         Field("id", Datatype.int64(), is_merge_key=True),
         Field("name", Datatype.string()),
@@ -15,7 +16,7 @@ def test_schema_values():
     assert len(values) == 2
     assert all(isinstance(v, Field) for v in values)
 
-def test_t():
+def test_schema_fields():
     # cwd = pathlib.Path.cwd()
     # csv_file_path = cwd / "data.csv"
     # ds = Dataset.from_csv(
@@ -25,7 +26,8 @@ def test_t():
         # merge_keys="msg_id"
     # )
 
-    tar_path = "./imagenet1k-train-0000.tar"
+    # tar_path = "../../../test_utils/resources/imagenet1k-train-0000.tar"
+    tar_path = "../../../test_utils/resources/test_wds.tar"
     dataset = Dataset.from_webdataset(
         name="test",
         file_uri=tar_path,
@@ -36,3 +38,25 @@ def test_t():
     assert "height" in dataset.fields
     assert "filename" in dataset.fields
     assert len(dataset.fields) == 4
+    # assert False
+
+# TODO: make dummy tar file instead of local file
+# test that the data is properly stored in new dataset
+def test_schema_data():
+    # tar_path = "../../../test_utils/resources/imagenet1k-train-0000.tar"
+    tar_path = "../../../test_utils/resources/test_wds.tar"
+
+    dataset = Dataset.from_webdataset(
+        name="test",
+        file_uri=tar_path,
+        merge_keys="filename"
+    )
+    # dataset.print()
+    # print(len(dataset))
+    records = dataset.scan().to_pydict()
+    for record in itertools.islice(records, 1):
+        assert record["label"] == 1
+        assert record["width"] == 500
+        assert record["height"] == 429
+        assert record["filename"] == "n01443537/n01443537_14753.JPEG"
+
