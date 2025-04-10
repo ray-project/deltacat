69e69e5c3853943d110425ef5ab441a83b510de6
Mingshi Peng <pmingshi@amazon.com>
2025-04-09 17:27:02 -0700

    feat: daft scan operator initial impl with hardcoded schema

--- deltacat/catalog/iceberg/impl.py
+++ deltacat/catalog/iceberg/impl.py
@@ -2,10 +2,13 @@ import logging
 
 from typing import Any, Dict, List, Optional, Union
 
-from daft import DataFrame
+from daft import DataFrame, context
+from daft.daft import ScanOperatorHandle, StorageConfig, PythonStorageConfig, NativeStorageConfig
+from daft.logical.builder import LogicalPlanBuilder
 
 from deltacat import logs
 from deltacat.catalog.model.table_definition import TableDefinition
+from deltacat.daft.daft_scan import DeltaCATScanOperator
 from deltacat.exceptions import TableAlreadyExistsError
 from deltacat.storage.iceberg.model import PartitionSchemeMapper, SchemaMapper
 from deltacat.storage.model.partition import PartitionScheme
@@ -146,7 +149,14 @@ def read_table(
     table: str, *args, namespace: Optional[str] = None, **kwargs
 ) -> DistributedDataset:
     """Read a table into a distributed dataset."""
-    raise NotImplementedError("read_table not implemented")
+    io_config = context.get_context().daft_planning_config.default_io_config
+    multithreaded_io = not context.get_context().is_ray_runner
+    storage_config = NativeStorageConfig(multithreaded_io, io_config)
+
+    dc_operator = DeltaCATScanOperator(table, namespace, StorageConfig.native(storage_config))
+    handle = ScanOperatorHandle.from_python_scan_operator(dc_operator)
+    builder = LogicalPlanBuilder.from_tabular_scan(scan_operator=handle)
+    return DataFrame(builder)
 
 def create_scan_plan(
     table: str,
---
+++ deltacat/daft/daft_scan.py
@@ -0,0 +1,81 @@
+from typing import Iterator
+
+from daft import Schema, DataType
+from daft.daft import StorageConfig, PartitionField, Pushdowns, ScanTask, PartitionTransform, FileFormatConfig, \
+    ParquetSourceConfig
+from daft.io.scan import ScanOperator, make_partition_field
+from daft.logical.schema import Field
+
+import deltacat
+
+
+class DeltaCATScanOperator(ScanOperator):
+    def __init__(self, table_name: str, namespace: str, storage_config: StorageConfig) -> None:
+        super().__init__()
+        self.table_name = table_name
+        self.namespace = namespace
+        self._schema = self._infer_schema()
+        self.partition_keys = self._infer_partition_keys()
+        self.storage_config = storage_config
+
+    def schema(self) -> Schema:
+        return self._schema
+
+    def display_name(self) -> str:
+        return f"DeltaCATScanOperator({self.namespace}.{self.table_name})"
+
+    def partitioning_keys(self) -> list[PartitionField]:
+        return self.partition_keys
+
+    def multiline_display(self) -> list[str]:
+        return [
+            self.display_name(),
+            f"Schema = {self._schema}",
+            f"Partitioning keys = {self.partitioning_keys}",
+            f"Storage config = {self.storage_config}",
+        ]
+
+    def to_scan_tasks(self, pushdowns: Pushdowns) -> Iterator[ScanTask]:
+        # TODO: implement pushdown predicate on DeltaCAT
+        dc_scan_plan = deltacat.create_scan_plan(self.table_name, self.namespace)
+        scan_tasks = []
+        file_format_config = FileFormatConfig.from_parquet_config(
+            # maybe this: ParquetSourceConfig(field_id_mapping=self._field_id_mapping)
+            ParquetSourceConfig()
+        )
+        for dc_scan_task in dc_scan_plan.scan_tasks:
+            for data_file in dc_scan_task.data_files():
+                st = ScanTask.catalog_scan_task(
+                    file=data_file.file_path,
+                    file_format= file_format_config,
+                    schema=self._schema._schema,
+                    storage_config=self.storage_config,
+                    pushdowns=pushdowns,
+                )
+                scan_tasks.append(st)
+        return iter(scan_tasks)
+
+
+    def can_absorb_filter(self) -> bool:
+        return False
+
+    def can_absorb_limit(self) -> bool:
+        return False
+
+    def can_absorb_select(self) -> bool:
+        return True
+
+    def _infer_schema(self) -> Schema:
+        # TODO: remove hard coding
+        fields: list[Field] = [
+            Field.create(name="symbol", dtype=DataType.string()),
+            Field.create(name="bid", dtype=DataType.float64()),
+            Field.create(name="ask", dtype=DataType.float64()),
+        ]
+        return Schema._from_fields(fields=fields)
+
+    def _infer_partition_keys(self) -> list[PartitionField]:
+        # TODO: remove hard coding
+        partition_field = Field.create(name="symbol_bucket", dtype=DataType.int32())
+        source_field = Field.create(name="symbol", dtype=DataType.string())
+        return [make_partition_field(field=partition_field, source_field=source_field, transform=PartitionTransform.iceberg_bucket(2))]
