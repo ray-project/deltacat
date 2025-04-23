435c39752a3544eff4fd39568925e1e0776303f8
Mingshi Peng <pmingshi@amazon.com>
2025-04-23 12:47:18 -0700

    chore: fix lint

--- deltacat/tests/storage/model/test_manifest.py
+++ deltacat/tests/storage/model/test_manifest.py
@@ -5,7 +5,7 @@ from deltacat.storage.model.manifest import Manifest
 
 @pytest.fixture
 def manifest_a():
-    return '''
+    return """
         {
           "entries":[
             {
@@ -29,7 +29,7 @@ def manifest_a():
                 "content_type":"application/x-amzn-unescaped-tsv",
                 "content_encoding":"gzip"
               }
-            }	
+            }
           ],
           "meta":{
             "record_count":0,
@@ -44,14 +44,15 @@ def manifest_a():
             "version":"1.0"
           }
         }
-           '''
+           """
+
 
 def test_manifest_from_json(manifest_a):
     manifest = Manifest.from_json(manifest_a)
 
     assert manifest.entries is not None
     assert len(manifest.entries) == 2
-    assert manifest.entries[0].uri == 's3://test_bucket/file1.tsv.gz'
+    assert manifest.entries[0].uri == "s3://test_bucket/file1.tsv.gz"
     assert manifest.entries[0].meta.record_count == 0
     assert manifest.meta.content_length == 579
-    assert manifest.author.name == 'Dave'
+    assert manifest.author.name == "Dave"
